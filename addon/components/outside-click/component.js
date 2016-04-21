b27111da6dad54575426693108e160c59e9f5243
Jason Tu <jasontu4@gmail.com>
2016-04-21 15:08:17 -0400

    should only call action when mousedown is outside

--- addon/components/outside-click/component.js
+++ addon/components/outside-click/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember'
 import layout from './template'
 import PropTypeMixin, { PropTypes } from 'ember-prop-types'
 
-const { K } = Ember
+const { K, $ } = Ember
 
 export default Ember.Component.extend(PropTypeMixin, {
   layout,
@@ -18,22 +18,32 @@ export default Ember.Component.extend(PropTypeMixin, {
 
   init() {
     this._super(...arguments)
-    this.handle = this.handle.bind(this)
+    this.handleDown = this.handleDown.bind(this)
+    this.handleUp = this.handleUp.bind(this)
   },
 
   didInsertElement() {
     this._super(...arguments)
-    document.addEventListener('click', this.handle, true)
+    document.addEventListener('mousedown', this.handleDown, true)
+    document.addEventListener('mouseup', this.handleUp, true)
   },
 
   willDestroyElement() {
     this._super(...arguments)
-    document.removeEventListener('click', this.handle, true)
+    document.removeEventListener('mousedown', this.handleDown, true)
+    document.removeEventListener('mouseup', this.handleUp, true)
   },
 
-  handle(e) {
+  isOutside: false,
+
+  handleDown(e) {
     const el = this.$()[0];
-    if (!el.contains(e.target)) this.get('onOutsideClick')(e)
+    if (!el.contains(e.target)) this.set('isOutside', true)
+  },
+
+  handleUp(e) {
+    if (this.get('isOutside')) this.get('onOutsideClick')(e)
+    this.set('isOutside', false)
   }
 })
 
